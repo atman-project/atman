bbd5a802fd74e48e3b9a8df9a87e10afa16272bd
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-06-29 01:00:03 +0900

    feat: add basic struct: running core and sending/receiving messages

---
+++ build_bindings.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -uexo pipefail
+
+PROJECT_NAME="atman_core"
+
+cbindgen -l C -o target/${PROJECT_NAME}.h
+
+cargo build --target aarch64-apple-ios
+lipo -info target/aarch64-apple-ios/debug/lib${PROJECT_NAME}.a
+
+cargo build --target x86_64-apple-ios
+lipo -info target/x86_64-apple-ios/debug/lib${PROJECT_NAME}.a
+
+lipo -create target/aarch64-apple-ios/debug/lib${PROJECT_NAME}.a \
+    target/x86_64-apple-ios/debug/lib${PROJECT_NAME}.a \
+    -output target/lib${PROJECT_NAME}.a
