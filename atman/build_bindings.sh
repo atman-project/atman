b8b6bfbdb0ccac6dd1a8cf8adaf0e4c255068c0f
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-10-08 10:22:07 +0900

    build: add comments in bindings.sh (#20)

--- atman/build_bindings.sh
+++ atman/build_bindings.sh
@@ -18,14 +18,17 @@ cbindgen -l C -o ${TARGET_DIR}/${PROJECT_NAME}.h
 
 LIB_NAME="lib${PROJECT_NAME}.a"
 
+# Build for ARM64 (iOS devices)
 cargo build --target aarch64-apple-ios $CARGO_FLAGS
 lipo -info ${TARGET_DIR}/aarch64-apple-ios/${BUILD_MODE}/${LIB_NAME}
 ls -lh ${TARGET_DIR}/aarch64-apple-ios/${BUILD_MODE}/${LIB_NAME}
 
+# Build for x86_64 (iOS simulator)
 cargo build --target x86_64-apple-ios $CARGO_FLAGS
 lipo -info ${TARGET_DIR}/x86_64-apple-ios/${BUILD_MODE}/${LIB_NAME}
 ls -lh ${TARGET_DIR}/x86_64-apple-ios/${BUILD_MODE}/${LIB_NAME}
 
+# Merge the two architectures into a single FAT library
 lipo -create ${TARGET_DIR}/aarch64-apple-ios/${BUILD_MODE}/${LIB_NAME} \
     ${TARGET_DIR}/x86_64-apple-ios/${BUILD_MODE}/${LIB_NAME} \
     -output ${TARGET_DIR}/${BUILD_MODE}/${LIB_NAME}
